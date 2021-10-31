ee5bde3f9d8c85c2735e631fbf5550061496fd66
meluchoMZ <miguelgodon@outlook.es>
2021-10-31 16:28:16 +0100

    [feature/authentication] - Added authentication by google and agendaly account

--- Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
+++ Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
@@ -9,7 +9,10 @@ package es.udc.psi.agendaly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 	Each change in the repository should be managed as follow:
 		- If it is a new feature, it should be developed on a branch called "feature/<feature_name>"
 		- If a bug is detected, a new issue should be added to github. The bug should be
-		  developed in a branch called "issue/<issue_id>"
+		  developed in a branch called "bug/<issue_id>"
 	Commits should be pushed to it's branch. Then, changes will be merged. After this, those
 	branches should be removed.
 	Commits should have the form: "[<bug/feature>#<id/name>] <commit message>"
@@ -29,5 +32,13 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button b = findViewById(R.id.feature_launcher);
+		b.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent auth = new Intent(getApplicationContext(), AuthenticationActivity.class);
+				startActivity(auth);
+			}
+		});
 	}
 }
