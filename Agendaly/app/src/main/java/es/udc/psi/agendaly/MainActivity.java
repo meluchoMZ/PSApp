b9e9cbc81298ca2daf0a84ac421c85ac69d503ac
laura <l.cabezas@udc.es>
2021-11-01 17:52:47 +0100

    fix dependencies

--- Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
+++ Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
@@ -9,7 +9,12 @@ package es.udc.psi.agendaly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import es.udc.psi.agendaly.TimeTable.Horario;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +34,14 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button l = findViewById(R.id.feature_launcherL);
+		l.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent horario = new Intent(getApplicationContext(), Horario.class);
+				startActivity(horario);
+			}
+		});
 	}
 }
