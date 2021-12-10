fc472b6544035ac6094c34c2d0b0ced4da00741f
Blanca <blanca.fernandez.martin@udc.es>
2021-12-10 20:39:56 +0100

    Revert "events can be added to the calendar"
    This reverts commit a2d7c2ed39e7346a742e4a47d99cf194d8dd8e93.

--- Agendaly/README.md
+++
@@ -1,5 +0,0 @@
-# PSApp (PS 2021/2022)
-
-Laura Cabezas González \\
-Blanca María Fernádez Martín \\
-Miguel Blanco Godón \\
--- Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
+++ Agendaly/app/src/main/java/es/udc/psi/agendaly/MainActivity.java
@@ -56,8 +56,8 @@ public class MainActivity extends BaseActivity
 		});
 
 
-		Button b = findViewById(R.id.feature_launcherL);
 
+		Button b = findViewById(R.id.feature_launcher);
 		b.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
